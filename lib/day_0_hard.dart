d27b1e9a81e801ee0f4763a6d05cbcd8ac0dcfa8
get6 <ittae.com@gmail.com>
2023-11-27 08:53:39 +0900

    Update day 0 question

--- lib/day_0_hard.dart
+++ lib/day_0_hard.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
 
 void main() => runApp(const MyApp());
 
@@ -47,220 +46,125 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     ShapeType shapeType =
         Random().nextInt(2) == 0 ? ShapeType.circle : ShapeType.square;
-    Widget dragWidget = shapeType == ShapeType.circle
-        ? CircleContainer(text: _dragText)
-        : SqureContainer(text: _dragText);
+    Widget dragWidget = ShapeContainer(text: _dragText, shapeType: shapeType);
 
-    return DraggableContainer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DragTarget(
-                  builder: (context, candidateData, rejectedData) {
-                    return SqureContainer(
-                      text: _squreCounter.toString(),
-                      width: 100,
-                      height: 100,
-                      strokeWidth: _squreStrokeWidth,
-                    );
-                  },
-                  onWillAcceptWithDetails: (details) {
-                    if (details.data == ShapeType.square) {
-                      setState(() {
-                        _squreStrokeWidth = 4;
-                      });
-                    }
-                    return true;
-                  },
-                  onAccept: (data) {
-                    if (data == ShapeType.square) {
-                      setState(() {
-                        _squreCounter++;
-                        _squreStrokeWidth = 2;
-                      });
-                    }
-                  },
-                ),
-                const SizedBox(width: 200),
-                DragTarget(
-                  builder: (context, candidateData, rejectedData) {
-                    return CircleContainer(
-                      text: _circleCounter.toString(),
-                      width: 100,
-                      height: 100,
-                      strokeWidth: _circleStrokeWidth,
-                    );
-                  },
-                  onWillAcceptWithDetails: (details) {
-                    if (details.data == ShapeType.circle) {
-                      setState(() {
-                        _circleStrokeWidth = 4;
-                      });
-                    }
-                    return true;
-                  },
-                  onAccept: (data) {
-                    if (data == ShapeType.circle) {
-                      setState(() {
-                        _circleCounter++;
-                        _circleStrokeWidth = 2;
-                      });
-                    }
-                  },
-                ),
-              ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DragTarget(
+                builder: (context, candidateData, rejectedData) {
+                  return ShapeContainer(
+                    text: _squreCounter.toString(),
+                    width: 100,
+                    height: 100,
+                    strokeWidth: _squreStrokeWidth,
+                    shapeType: ShapeType.square,
+                  );
+                },
+                onWillAcceptWithDetails: (details) {
+                  if (details.data == ShapeType.square) {
+                    setState(() {
+                      _squreStrokeWidth = 4;
+                    });
+                  }
+                  return true;
+                },
+                onAccept: (data) {
+                  if (data == ShapeType.square) {
+                    setState(() {
+                      _squreCounter++;
+                      _squreStrokeWidth = 2;
+                    });
+                  }
+                },
+              ),
+              const SizedBox(width: 200),
+              DragTarget(
+                builder: (context, candidateData, rejectedData) {
+                  return ShapeContainer(
+                    text: _circleCounter.toString(),
+                    width: 100,
+                    height: 100,
+                    strokeWidth: _circleStrokeWidth,
+                    shapeType: ShapeType.circle,
+                  );
+                },
+                onWillAcceptWithDetails: (details) {
+                  if (details.data == ShapeType.circle) {
+                    setState(() {
+                      _circleStrokeWidth = 4;
+                    });
+                  }
+                  return true;
+                },
+                onAccept: (data) {
+                  if (data == ShapeType.circle) {
+                    setState(() {
+                      _circleCounter++;
+                      _circleStrokeWidth = 2;
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+        Draggable(
+          data: shapeType,
+          feedback: dragWidget,
+          childWhenDragging: const SizedBox(height: 80),
+          child: dragWidget,
+        ),
+        const SizedBox(height: 40),
+      ],
+    );
+  }
+}
+
+class ShapeContainer extends StatelessWidget {
+  const ShapeContainer({
+    super.key,
+    required this.text,
+    this.width = 80,
+    this.height = 80,
+    this.strokeWidth = 2,
+    required this.shapeType,
+  });
+  final double width;
+  final double height;
+  final String text;
+  final double strokeWidth;
+  final ShapeType shapeType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: const EdgeInsets.all(8),
+      decoration: shapeType == ShapeType.circle
+          ? BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(width: strokeWidth),
+            )
+          : BoxDecoration(
+              border: Border.all(width: strokeWidth),
+              borderRadius: BorderRadius.circular(8),
             ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
           ),
-          Draggable(
-            data: shapeType,
-            feedback: dragWidget,
-            childWhenDragging: const SizedBox(height: 80),
-            child: dragWidget,
-          ),
-          const SizedBox(height: 40),
-        ],
-      ),
-    );
-  }
-}
-
-class CircleContainer extends StatelessWidget {
-  const CircleContainer({
-    super.key,
-    required this.text,
-    this.width = 80,
-    this.height = 80,
-    this.strokeWidth = 2,
-  });
-  final double width;
-  final double height;
-  final String text;
-  final double strokeWidth;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(width: strokeWidth),
-      ),
-      child: Center(child: Text(text)),
-    );
-  }
-}
-
-class SqureContainer extends StatelessWidget {
-  const SqureContainer({
-    super.key,
-    required this.text,
-    this.width = 80,
-    this.height = 80,
-    this.strokeWidth = 2,
-  });
-  final double width;
-  final double height;
-  final String text;
-  final double strokeWidth;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        border: Border.all(width: strokeWidth),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(child: Text(text)),
-    );
-  }
-}
-
-class DraggableContainer extends StatefulWidget {
-  const DraggableContainer({super.key, required this.child});
-  final Widget child;
-
-  @override
-  State<DraggableContainer> createState() => _DraggableContainerState();
-}
-
-class _DraggableContainerState extends State<DraggableContainer>
-    with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Alignment _dragAlignment = Alignment.center;
-  Animation<Alignment>? _animation;
-
-  void _runAnimation(Offset pixelsPerSecond, Size size) {
-    _animation = _controller?.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment.center,
-      ),
-    );
-    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
-    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
-    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
-    final unitVelocity = unitsPerSecond.distance;
-
-    const spring = SpringDescription(
-      mass: 30,
-      stiffness: 1,
-      damping: 1,
-    );
-
-    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
-
-    _controller!.animateWith(simulation);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-
-    _controller?.addListener(() {
-      setState(() {
-        _dragAlignment = _animation!.value;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return GestureDetector(
-      onPanDown: (details) {
-        _controller?.stop();
-      },
-      onPanUpdate: (details) {
-        setState(() {
-          _dragAlignment += Alignment(
-            details.delta.dx / (size.width / 2),
-            details.delta.dy / (size.height / 2),
-          );
-        });
-      },
-      onPanEnd: (details) {
-        _runAnimation(details.velocity.pixelsPerSecond, size);
-      },
-      child: Align(
-        alignment: _dragAlignment,
-        child: widget.child,
+        ),
       ),
     );
   }
